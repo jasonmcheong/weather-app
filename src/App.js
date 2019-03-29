01d85f3a7f3f03537767ea04ef3b82cd8099cdb8
Jason <jasonmcheong@gmail.com>
2019-03-29 12:21:58 -0600

    successfully fetching data and displaying it

--- src/App.js
+++ src/App.js
@@ -5,40 +5,43 @@ import Header from './components/header';
 import WeatherDisplay from './components/weather-display';
 
 class App extends Component {
-    state = {
-        locationList: [],
-        locationObj: [],
-    };
+	state = {
+		locationList: [],
+		locationArr: [],
+	};
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.handleLocation = this.handleLocation.bind(this);
-        this.handleLocationID = this.handleLocationID.bind(this);
-    }
+		this.handleLocation = this.handleLocation.bind(this);
+		this.handleLocationID = this.handleLocationID.bind(this);
+	}
 
-    // gets the location array from <Header/>
-    handleLocation(value) {
-        this.setState({
-            locationList: value,
-        });
-    }
+	// gets the location array from <Header/>
+	handleLocation(value) {
+		this.setState({
+			locationList: value,
+		});
+	}
 
-    handleLocationID(value) {
-        this.setState({
-            locationObj: value,
-        });
-    }
+	handleLocationID(value) {
+		this.setState({
+			locationArr: value,
+		});
+	}
 
-    render() {
-        return (
-            <div className='App'>
-                <Header handleLocation={this.handleLocation} />
-                <LocationList locationList={this.state.locationList} getLocationID={this.handleLocationID} />
-                <WeatherDisplay weather={this.state.locationObj} />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<Header handleLocation={this.handleLocation} />
+				<LocationList
+					locationList={this.state.locationList}
+					getLocationID={this.handleLocationID}
+				/>
+				<WeatherDisplay temperature={this.state.locationArr} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/weather-display.js
+++ src/components/weather-display.js
@@ -6,8 +6,28 @@
 
 import React from 'react';
 
-const WeatherDisplay = props => {
-    return <div>{props.weather.title}</div>;
+const imgURL = 'https://www.metaweather.com/static/img/weather/';
+const imgExt = '.svg';
+
+const WeatherDisplay = (props) => {
+	return (
+		<div className="weather-display">
+			<h2>
+				{props.temperature.map((temp) => {
+					return (
+						<div key={temp.id}>
+							<img src={`${imgURL}${temp.weather_state_abbr}${imgExt}`} alt="" />
+							<h3>{temp.weather_state_name}</h3>
+							<p>{temp.applicable_date}</p>
+							<h2>{temp.the_temp}</h2>
+							<p>{temp.min_temp}</p>
+							<p>{temp.max_temp}</p>
+						</div>
+					);
+				})}
+			</h2>
+		</div>
+	);
 };
 
 export default WeatherDisplay;
