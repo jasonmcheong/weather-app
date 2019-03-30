d0d37f42c70f9c90f0498815ff77b46ec8dfa499
Jason Cheong <jasonmcheong@gmail.com>
2019-03-30 11:36:23 -0600

    finished styling the application

--- src/components/weather-display.js
+++ src/components/weather-display.js
@@ -1,40 +1,31 @@
-/*
- *
- *   TODO: display the weather when the component receives props
- *
- */
-
 import React from 'react';
 
 const imgURL = 'https://www.metaweather.com/static/img/weather/';
 const imgExt = '.svg';
 
-const WeatherDisplay = (props) => {
+const WeatherDisplay = props => {
     return (
-        <div className="weather-display">
-            <h2>
-                {props.temperature.map((temp) => {
-                    return (
-                        <div key={temp.id} className="panel">
-                            <div className="status">
-                                <img
-                                    src={`${imgURL}${
-                                        temp.weather_state_abbr
-                                    }${imgExt}`}
-                                    alt=""
-                                />
-                                <h3>{temp.weather_state_name}</h3>
-                            </div>
-                            <div className="info">
-                                <h2>{temp.applicable_date}</h2>
-                                <p>{temp.the_temp}</p>
-                                <p>{temp.min_temp}</p>
-                                <p>{temp.max_temp}</p>
+        <div className='weather-display'>
+            {props.temperature.map(temp => {
+                return (
+                    <div key={temp.id} className='panel'>
+                        <div className='status'>
+                            <img src={`${imgURL}${temp.weather_state_abbr}${imgExt}`} alt='' />
+                            <h3>{temp.weather_state_name}</h3>
+                        </div>
+                        <div className='info'>
+                            <h2>{temp.applicable_date}</h2>
+                            <div className='temperature-container'>
+                                <p className='temperature'>{temp.the_temp.toFixed(0)}&deg;C</p>
+                                <div className='hi-lo'>
+                                    <h2 className='hi'>{temp.max_temp.toFixed(0)}&deg;C</h2>
+                                    <h2 className='lo'>{temp.min_temp.toFixed(0)}&deg;C</h2>
+                                </div>
                             </div>
                         </div>
-                    );
-                })}
-            </h2>
+                    </div>
+                );
+            })}
         </div>
     );
 };
